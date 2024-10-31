da83347ff00bdef96e8c2d068726f13563361c7f
Jake Goritski <jgoritski@headspin.io>
2024-10-31 11:38:09 -0700

    update quotes

--- jake.gorit.ski/assets/js/quotes.js
+++ jake.gorit.ski/assets/js/quotes.js
@@ -3,6 +3,7 @@ const quotes = [
   "I thought what I'd do was, I'd pretend I was one of those deaf-mutes.",
   "Peace comes from within, do not seek it without.",
   "Truth suffers from too much analysis.",
+  "All of which makes me anxious. At times, unbearably so.",
 ];
 document.addEventListener("DOMContentLoaded", (event) => {
   let el = document.getElementById("subtitle");
