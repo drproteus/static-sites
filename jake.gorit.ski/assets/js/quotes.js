a9e8bd38608e3490ec98e5752d305be448a79f76
Jake Goritski <jgoritski@headspin.io>
2024-10-31 15:25:50 -0700

    quote index query param

--- jake.gorit.ski/assets/js/quotes.js
+++ jake.gorit.ski/assets/js/quotes.js
@@ -4,9 +4,17 @@ const quotes = [
   "Peace comes from within, do not seek it without.",
   "Truth suffers from too much analysis.",
   "All of which makes me anxious. At times, unbearably so.",
+  "I was a victim of a series of accidents, as are we all.",
 ];
 document.addEventListener("DOMContentLoaded", (event) => {
   let el = document.getElementById("subtitle");
-  let i = Math.floor(Math.random() * quotes.length);
+  let params = new URLSearchParams(window.location.search);
+  let i;
+  if (params.get("q") !== null) {
+    // Use quote index from query.
+    i = Math.min(Math.max(Number(params.get("q")), 0), quotes.length - 1);
+  } else {
+    i = Math.floor(Math.random() * quotes.length);
+  }
   el.innerHTML = quotes[i];
 });
