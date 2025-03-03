e02e91c91be49a66e9d7c07420f0c6123dcff712
Jake Goritski <jgoritski@headspin.io>
2025-03-03 10:53:06 -0800

    update quotes

--- jake.gorit.ski/assets/js/quotes.js
+++ jake.gorit.ski/assets/js/quotes.js
@@ -5,7 +5,17 @@ const quotes = [
   "Truth suffers from too much analysis.",
   "All of which makes me anxious. At times, unbearably so.",
   "I was a victim of a series of accidents, as are we all.",
+  "I'll kick you apart.",
+  "How strange it is to be anything at all.",
+  "All you've got are your actions, man.",
+  "Did they get you to trade your heroes for ghosts?",
 ];
+
+function updateQuote(i) {
+  let el = document.getElementById("subtitle");
+  el.innerHTML = quotes[i];
+};
+
 document.addEventListener("DOMContentLoaded", (event) => {
   let el = document.getElementById("subtitle");
   let params = new URLSearchParams(window.location.search);
@@ -16,5 +26,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
   } else {
     i = Math.floor(Math.random() * quotes.length);
   }
-  el.innerHTML = quotes[i];
+  window.currQuoteIdx = i;
+  updateQuote(i);
+  el.addEventListener("click", (event) => {
+    // let i = Math.floor(Math.random() * quotes.length);
+    window.currQuoteIdx = (window.currQuoteIdx + 1) % quotes.length;
+    updateQuote(window.currQuoteIdx);
+  });
 });
+
